f0d51a000f8bd2a58fe7e047e458b829d1946aff
jack <jack68@naver.com>
2024-02-10 00:03:26 +0900

    first commit

---
+++ src/main/java/org/example/config/SwaggerConfig.java
@@ -0,0 +1,47 @@
+package org.example.config;
+
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.web.servlet.config.annotation.EnableWebMvc;
+import org.springframework.web.servlet.config.annotation.ResourceHandlerRegistry;
+import org.springframework.web.servlet.config.annotation.WebMvcConfigurer;
+import org.springframework.web.servlet.config.annotation.WebMvcConfigurerAdapter;
+import springfox.documentation.builders.ApiInfoBuilder;
+import springfox.documentation.builders.PathSelectors;
+import springfox.documentation.builders.RequestHandlerSelectors;
+import springfox.documentation.service.ApiInfo;
+import springfox.documentation.spi.DocumentationType;
+import springfox.documentation.spring.web.plugins.Docket;
+import springfox.documentation.swagger2.annotations.EnableSwagger2WebMvc;
+
+@Configuration
+@EnableSwagger2WebMvc
+public class SwaggerConfig extends WebMvcConfigurerAdapter {
+
+    private ApiInfo apiInfo() {
+
+        return new ApiInfoBuilder()
+                .title("Demo")
+                .description("API EXAMPLE")
+                .build();
+    }
+
+    @Bean
+    public Docket commonApi() {
+        return new Docket(DocumentationType.SWAGGER_2)
+                .groupName("example")
+                .apiInfo(this.apiInfo())
+                .select()
+                .apis(RequestHandlerSelectors
+                        .basePackage("org.example"))
+                .paths(PathSelectors.ant("/**"))
+                .build();
+    }
+
+    @Override
+    public void addResourceHandlers(ResourceHandlerRegistry registry) {
+        registry.addResourceHandler("swagger-ui.html")
+                .addResourceLocations("classpath:/META-INF/resources/");
+    }
+}
+
